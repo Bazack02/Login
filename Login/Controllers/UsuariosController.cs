1f4974479df970c0ec33b38783edfea53810b313
JOAO BAZACA <joao.bazaca@fatec.sp.gov.br>
2022-05-06 21:58:48 -0300

    Adicionar arquivos de projeto.

---
+++ Login/Controllers/UsuariosController.cs
@@ -0,0 +1,48 @@
+﻿using Login.Entidades;
+using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Contexto db;
+
+        public UsuariosController(Contexto contexto)
+        {
+            db = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.USUARIOS.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario dadosTela)
+        {
+            if (string.IsNullOrEmpty(dadosTela.Login))
+            {
+                TempData["erro"] = "Campo Login não pode estar em branco";
+            }
+
+            if (string.IsNullOrEmpty(dadosTela.Senha))
+            {
+                TempData["erro"] = "Campo Senha não pode ser branco";
+                return View();
+            }
+            db.USUARIOS.Add(dadosTela);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
